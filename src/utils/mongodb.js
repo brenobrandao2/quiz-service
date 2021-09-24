8dd4485f556ca455830522082d1110ee6de50508
brenobrandao2 <breno.brandao@prodap.com.br>
2021-09-24 19:37:02 -0300

    feature: Update mongodb.js

--- src/utils/mongodb.js
+++ src/utils/mongodb.js
@@ -1,9 +1,20 @@
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb://localhost/quiz')
+export class MongoDb {
+    constructor() {
+        this.uri = 'mongodb://localhost/life_and_money_quiz'
+        this.options =  { useNewUrlParser: true }
+    }
 
-mongoose.connection.once('open', () => {
-    console.log('connection has been made')
-}).on('error', (error) => {
-    console.log('error: ', error)
-})
+    newConnection() {
+        mongoose.connect(this.uri, this.options)
+        const db = mongoose.connection.once('open', () => {
+            console.log('connection has been made')
+        }).on('error', (error) => {
+            console.log('error: ', error)
+        })
+
+        console.log(db)
+        return db
+    }
+}
