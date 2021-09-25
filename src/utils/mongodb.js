8646d1ae3dae95d168efb43e216c8c41f9ad0b13
brenobrandao2 <breno.brandao@prodap.com.br>
2021-09-25 01:13:26 -0300

    feature: Atualiza conexão com bd

--- src/utils/mongodb.js
+++ src/utils/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export default class MongoDb {
     constructor() {
-        this.uri = 'mongodb://localhost/life_and_money_quiz'
+        this.uri = 'mongodb://lam_quiz:HndC$1087Lam%402020a@localhost/life_and_money_quiz'
         this.options =  { useNewUrlParser: true }
         
     }
