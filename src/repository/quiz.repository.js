870ff1b5d2eebf7c64bb0f7692932af41f26b946
brenobrandao2 <breno.brandao@prodap.com.br>
2021-10-07 16:59:00 -0300

    refactor: Remove parte do cliente

--- src/repository/quiz.repository.js
+++ src/repository/quiz.repository.js
@@ -174,32 +174,3 @@ export const update = async (quiz) => {
 //         })
 //     })
 // }
-
-export const getToken = async (_id) => {
-    return new Promise((resolve, reject) => {
-        try {
-            console.log('Buscando token por id do quiz')
-            const objectId = new mongoose.Types.ObjectId(_id)
-            db.mongo.connect(db.uri, async (error, db) => {
-                if (error)
-                    return reject(error)
-                
-                console.log(objectId)
-                db.collection(QUIZ_COLLECTION).find({ "_id": objectId }).toArray((error, result) => {  
-                    if (error) {
-                        console.log('Falha ao buscar token do quiz')   
-                        reject(error)
-                    } else {
-                        console.log('Sucesso ao buscar token do quiz')
-                        const token = result[0].token
-                        resolve(token)
-                    }
-                    db.close()
-                  })
-            })
-        } catch (error) {
-            console.log('Falha ao buscar token do quiz')
-            reject(error)
-        }
-    })
-}
